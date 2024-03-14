42ba83b2ae14b2b27882fe5dd972c13e8d027ae6
baderlt <latrachbadr9@gmail.com>
2024-03-14 18:13:58 +0000

    add option opening broser headless

--- Frontend/src/Compoment/ListEmails.jsx
+++ Frontend/src/Compoment/ListEmails.jsx
@@ -15,13 +15,15 @@ const [copy,setcopy]=useState(false);
       }
 return (
     <>
+    {Emails.length > 0 ? 
     <div className="Emails relative bg-white text-black rounded-lg flex flex-col text-left px-5 py-5 h-auto max-h-[300px] overflow-y-auto ">
-        <span className={` absolute top-0 right-0 ${!copy ? "bg-slate-300" :'bg-green-200' }   rounded m-2 p-1 hover:bg-gray-400 cursor-pointer flex`} onClick={Copy_Emails}> <b>{copy ? 'Cpyed':'Copy'}</b> &ensp;<img src="Copy.png" alt="copy"  /></span>
-       {Emails.length > 0 ? Emails.map((email,index)=>{
+        <span className={` absolute top-0 right-0 ${!copy ? "bg-slate-300" :'bg-slate-500' }   rounded m-2 p-1 hover:bg-gray-400 cursor-pointer flex`} onClick={Copy_Emails}> <b>{copy ? 'Cpyed':'Copy'}</b> &ensp;<img src="Copy.png" alt="copy"  /></span>
+      { Emails.map((email,index)=>{
         
-        return <p key={index} className="flex"><span className="index-email  text-gray-500">{index}</span>&ensp;&ensp;<p className="email" >{email}</p> </p>
-        }) :""}
+        return <p key={index} className="flex"><span className="index-email  text-gray-500">{index}</span>&ensp;&ensp;<p className="email" >{email}</p> </p>;
+})}
 </div>
+ : <h2 className="text-xl">No Email Found .. !</h2>}
     </>
 )
 }
