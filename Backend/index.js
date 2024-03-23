1c6769433dc2daeca48f98f10942fb6d094ac081
baderlt <latrachbadr9@gmail.com>
2024-03-23 23:24:09 +0000

    otpm

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import googleSearch from './Scraper.js';
 import download from './download_txt.js';
+import { launch } from 'chrome-launcher';
+
 const PORT=process.env.PORT || 5000 
 dotenv.config();
 const app = express();
@@ -10,8 +12,15 @@ app.use(cors());
 app.use(express.json());
 app.post('/api/EmailScraping',googleSearch);
 app.get('/api/downloadfile/:file',download)
-app.get('/api/test',(req,res)=>{
-    res.send('Hi ');
+app.get('/api/test',async (req,res)=>{
+
+
+    const chrome = await launch({
+        startingUrl: 'https://google.com'
+    });
+    console.log('Chrome debuggable on port:', chrome.port);
+    console.log('Executable path:', chrome.executablePath);
+
 })
 app.listen(PORT,(req,res)=>{
     console.log(`server runing on port:${PORT} `)
