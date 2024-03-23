6581d122a9625c3aa1fd8c82180b97473e3ae49e
baderlt <latrachbadr9@gmail.com>
2024-03-23 23:38:37 +0000

    add: deploy workflow

--- Frontend/src/touls.jsx
+++ Frontend/src/touls.jsx
@@ -1,2 +1,2 @@
 // export const BaseUrl='http://localhost:5000/api';
-export const BaseUrl='https://email-scraper-ayc1.onrender.com/api';
+export const BaseUrl='https://email-scraper-skz9.onrender.com/api';
