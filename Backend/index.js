3b432fe7370ce458341f2cdae76a742ff2651a8c
Bader Latarche <122647283+baderlt@users.noreply.github.com>
2024-04-24 23:06:30 +0100

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,8 +5,8 @@ import googleSearch from './Scraper.js';
 import download from './download_txt.js';
 
 const PORT=process.env.PORT || 5000 
-
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
