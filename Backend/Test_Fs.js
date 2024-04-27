0ddc5161bd914aac5bf6f7bc1790b8c80321dc93
baderlt <baderlatrache10@gmail.com>
2024-04-27 22:48:00 +0100

    optm

--- Backend/Test_Fs.js
+++ Backend/Test_Fs.js
@@ -10,11 +10,30 @@ let content=['bader','amine','yassin'];
 //         console.log(error);
 //     }else{console.log('trucate')};
 // });
-fs.appendFileSync('test.txt',content[0],(err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('write');
-    }
+// fs.appendFileSync('test.txt',content[0],(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('write');
+//     }
+// });
+
+///// read file with fs 
+
+// Create a readable stream
+var reader = fs.createReadStream('test.txt', 'utf-8');
+
+// Event listener for when data is available
+reader.on('data', function(chunk) {
+    // Split the chunk by lines
+    var lines = chunk.split('\n');
+    // Loop through each line
+    lines.forEach(function(line) {
+        console.log(line); // Process each line as needed
+    });
 });
-console.log('hi');
+
+// Event listener for when all data has been read
+reader.on('end', function() {
+    console.log('End of file');
+});
