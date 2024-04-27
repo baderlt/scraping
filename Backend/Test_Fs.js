a055f01b8f25f3fe98b86ba6411bc71b3dc93d4a
baderlt <baderlatrache10@gmail.com>
2024-04-27 22:19:05 +0100

    optm

--- Backend/Test_Fs.js
+++ Backend/Test_Fs.js
@@ -1,1 +1,8 @@
-import fs from 'fs';
+import fs from "fs";
+//// write in  file with fs
+let content=['bader','amine','yassin'];
+var  writeStram=fs.createWriteStream('test.txt','u');
+
+content.forEach((item)=>writeStram.write(item+'\n')); 
+writeStram.write(content[0]);
+console.log('hi');
